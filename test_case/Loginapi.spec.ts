74e4a5a37568103f61f302ebdecc734d4dbcdc6a
Mohanraj <mohanmraj@rocketmail.com>
2026-07-15 13:21:12 +0530

    beforeall api testing

--- test_case/Loginapi.spec.ts
+++ test_case/Loginapi.spec.ts
@@ -1,50 +1,8 @@
 import { expect, test } from '@playwright/test';
 
-// test('Create Token', async ({ request }) => {
-// const response = await request.post('https://restful-booker.herokuapp.com/auth', {
-//     headers: {
-//         'Content-Type': 'application/json'
-//     },
-//     data: {
-//         username: 'admin',
-//         password: 'password123'
-//     }
-// })
-// const responseBody = await response.json();
-// console.log(responseBody.token);
-// })
-
-// test('Create Booking', async ({ request }) => {
-// const response = await request.post('https://restful-booker.herokuapp.com/booking', {
-//     headers: {
-//         'Content-Type': 'application/json'
-//     },
-//     data: {
-//         "firstname" : "Mohanraj",
-//         "lastname" : "Mano",
-//         "totalprice" : 111,
-//         "depositpaid" : true,
-//         "bookingdates" : {
-//             "checkin" : "2018-01-01",
-//             "checkout" : "2019-01-01"
-//         },
-//         "additionalneeds" : "Breakfast"
-//     }
-// })
-// const responseBody = await response.json();
-// console.log(responseBody);
-//})
-
-// test('Get Booking', async ({ request }) => {
-//     const response = await request.get('https://restful-booker.herokuapp.com/booking/10523');
-//     expect(response.status()).toBe(200);
-
-//     const responseBody = await response.json();
-//     console.log(responseBody);
-// });
-
-test('Update Booking', async ({ request }) => {
-    const tokenresponse = await request.post ('https://restful-booker.herokuapp.com/auth', {
+let token: string;
+test.beforeAll('Create Token', async ({ request }) => {
+const response = await request.post('https://restful-booker.herokuapp.com/auth', {
     headers: {
         'Content-Type': 'application/json'
     },
@@ -52,15 +10,47 @@ test('Update Booking', async ({ request }) => {
         username: 'admin',
         password: 'password123'
     }
+})
+const responseBody = await response.json();
+token = responseBody.token;
+})
+
+test('Create Booking', async ({ request }) => {
+const response = await request.post('https://restful-booker.herokuapp.com/booking', {
+    headers: {
+        'Content-Type': 'application/json'
+    },
+    data: {
+        "firstname" : "Mohanraj",
+        "lastname" : "Mano",
+        "totalprice" : 111,
+        "depositpaid" : true,
+        "bookingdates" : {
+            "checkin" : "2018-01-01",
+            "checkout" : "2019-01-01"
+        },
+        "additionalneeds" : "Breakfast"
+    }
+})
+const responseBody = await response.json();
+console.log(responseBody);
+})
+
+test('Get Booking', async ({ request }) => {
+    const response = await request.get('https://restful-booker.herokuapp.com/booking/10523');
+    expect(response.status()).toBe(200);
+
+    const responseBody = await response.json();
+    console.log(responseBody);
+});
+
+test('Update Booking', async ({ request }) => {
     
-        
-}) 
-    const ltoken = await tokenresponse.json();
 
     const response = await request.put("https://restful-booker.herokuapp.com/booking/:10523", {
         headers: {
             'content-type': 'application/json',
-            'Cookie': `token = ${ltoken.token}`
+            'Cookie': `token = +token`
         },
         data: {
             "firstname": "Mani",
