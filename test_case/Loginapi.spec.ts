323bd02c1b0353d4f881ab27dde1352cd8791fba
Mohanraj <mohanmraj@rocketmail.com>
2026-07-15 12:04:16 +0530

    Get Booking the booking details of last create booking id 4723

--- test_case/Loginapi.spec.ts
+++ test_case/Loginapi.spec.ts
@@ -14,23 +14,29 @@ import {test} from '@playwright/test';
 // console.log(responseBody.token);
 // })
 
-test('Create Booking', async ({ request }) => {
-const response = await request.post('https://restful-booker.herokuapp.com/booking', {
-    headers: {
-        'Content-Type': 'application/json'
-    },
-    data: {
-        "firstname" : "Mohanraj",
-        "lastname" : "Manogaran",
-        "totalprice" : 111,
-        "depositpaid" : true,
-        "bookingdates" : {
-            "checkin" : "2018-01-01",
-            "checkout" : "2019-01-01"
-        },
-        "additionalneeds" : "Breakfast"
-    }
-})
-const responseBody = await response.json();
-console.log(responseBody);
-})
+// test('Create Booking', async ({ request }) => {
+// const response = await request.post('https://restful-booker.herokuapp.com/booking', {
+//     headers: {
+//         'Content-Type': 'application/json'
+//     },
+//     data: {
+//         "firstname" : "Mohanraj",
+//         "lastname" : "Manogaran",
+//         "totalprice" : 111,
+//         "depositpaid" : true,
+//         "bookingdates" : {
+//             "checkin" : "2018-01-01",
+//             "checkout" : "2019-01-01"
+//         },
+//         "additionalneeds" : "Breakfast"
+//     }
+// })
+// const responseBody = await response.json();
+// console.log(responseBody);
+// })
+
+test('Get Booking', async ({ request }) => {
+    const response = await request.get('https://restful-booker.herokuapp.com/booking/4723')
+    const responseBody = await response.json();
+    console.log(responseBody);}
+    )
