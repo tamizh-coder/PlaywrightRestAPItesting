075a9c748b6ba00f3c4d7e153b298dc78dfc2e35
Mohanraj <mohanmraj@rocketmail.com>
2026-07-15 22:45:44 +0530

    ajv moudle validation

--- test_case/schema.spec.ts
+++ test_case/schema.spec.ts
@@ -1,4 +1,6 @@
 import {test, expect} from '@playwright/test';
+import Ajv from 'ajv';  
+import { validateAdditionalItems } from 'ajv/dist/vocabularies/applicator/additionalItems';
 
 test('Api schema validation', async({request})=> {
 const BookingPayload ={
@@ -13,12 +15,46 @@ const BookingPayload ={
     "additionalneeds" : "Breakfast"
 }
 
-    const response = await request.post('/booking',{
+    const bookingshcema = {
+        "$schema": "http://json-schema.org/draft-07/schema#",
+        "type": "object",
+        "properties": {
+            "bookingid": { "type": "integer" },
+            "booking": {
+                "type": "object",
+                "properties": {
+                    "firstname": { "type": "string" },
+                    "lastname": { "type": "string" },
+                    "totalprice": { "type": "integer" },
+                    "depositpaid": { "type": "boolean" },
+                    "bookingdates": {
+                        "type": "object",
+                        "properties": {
+                            "checkin": { "type": "string" },
+                            "checkout": { "type": "string" }
+                        },
+                        "required": ["checkin", "checkout"]
+                    },
+                    "additionalneeds": { "type": "string" }
+                },
+                "required": ["firstname", "lastname", "totalprice", "depositpaid", "bookingdates", "additionalneeds"]
+            }
+        },
+        "required": ["bookingid", "booking"]
+    };
+
+    const response = await request.post('/booking',{ 
         headers:{
-            'conternt-type':'application/json'
+            'Content-Type':'application/json'
         },
         data:BookingPayload
     });
     const responseBody = await response.json();
-    console.log(responseBody);
+    //console.log(responseBody);
+    const ajv = new Ajv();
+    const validate = ajv.compile(bookingshcema);
+    const isValid = validate(responseBody);
+    console.log(isValid);
+
+
 });
