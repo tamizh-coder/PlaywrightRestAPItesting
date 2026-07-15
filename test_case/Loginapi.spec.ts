b757cb55cb303724c97ecf85825a1901d9517327
Mohanraj <mohanmraj@rocketmail.com>
2026-07-15 11:54:21 +0530

    create booking test case added

--- test_case/Loginapi.spec.ts
+++ test_case/Loginapi.spec.ts
@@ -1,18 +1,36 @@
 import {test} from '@playwright/test';
 
-test('Create Token', async ({ request }) => {
-const response = await request.post('https://restful-booker.herokuapp.com/auth', {
+// test('Create Token', async ({ request }) => {
+// const response = await request.post('https://restful-booker.herokuapp.com/auth', {
+//     headers: {
+//         'Content-Type': 'application/json'
+//     },
+//     data: {
+//         username: 'admin',
+//         password: 'password123'
+//     }
+// })
+// const responseBody = await response.json();
+// console.log(responseBody.token);
+// })
+
+test('Create Booking', async ({ request }) => {
+const response = await request.post('https://restful-booker.herokuapp.com/booking', {
     headers: {
         'Content-Type': 'application/json'
     },
     data: {
-        username: 'admin',
-        password: 'password123'
+        "firstname" : "Mohanraj",
+        "lastname" : "Manogaran",
+        "totalprice" : 111,
+        "depositpaid" : true,
+        "bookingdates" : {
+            "checkin" : "2018-01-01",
+            "checkout" : "2019-01-01"
+        },
+        "additionalneeds" : "Breakfast"
     }
 })
 const responseBody = await response.json();
-console.log(responseBody.token);
-
-
-
+console.log(responseBody);
 })
