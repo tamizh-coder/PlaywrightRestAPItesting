9c15949d5f51c66eeb6ca14d8cda890bf3ca227e
Mohanraj <mohanmraj@rocketmail.com>
2026-07-16 21:55:39 +0530

    directly as json object. The schema defines the expected structure of the response, including the types of each property and which properties are required.

---
+++ test_case/payload.spec.ts
@@ -0,0 +1,22 @@
+import {test} from '@playwright/test';
+test('Upload Payload', async({request})=> {
+    const response = await request.post('/booking',{
+        headers:{
+            'Content-Type':'application/json'
+        },
+        data:{
+
+            "firstname" : "Jim",
+    "lastname" : "Brown",
+    "totalprice" : 111,
+    "depositpaid" : true,
+    "bookingdates" : {
+        "checkin" : "2018-01-01",
+        "checkout" : "2019-01-01"
+    },
+    "additionalneeds" : "Breakfast"
+
+        }
+})
+console.log(await response.json())
+})
