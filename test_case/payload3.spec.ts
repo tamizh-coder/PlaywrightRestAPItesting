c7a583cb4b7f9f005f5cf49d26e16603ac8f2baa
Mohanraj <mohanmraj@rocketmail.com>
2026-07-17 18:58:45 +0530

    payload from json file

--- test_case/payload3.spec.ts
+++ test_case/payload3.spec.ts
@@ -1,15 +1,15 @@
 //from json file from import
 import { request, test } from "@playwright/test";
+import booking from '../Payload/bookingpayload.json';
 
 test('jsonfile  import', async ({request})=>{
 
-    const response = await request.post('/booking');
+    const response = await request.post('/booking',{
     headers:{
-        'Content-Types';'applications/json'
+        'Content-Types':'application/json'
     },
-    data:{
-        
-    }
-
-
+    data:booking
 })
+
+
+});
