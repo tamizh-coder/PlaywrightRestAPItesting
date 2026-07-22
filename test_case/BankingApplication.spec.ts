0accfc2abf378a49fb3d985913fc2d386f3a0b73
Mohanraj <mohanmraj@rocketmail.com>
2026-07-22 14:43:45 +0530

    new clsss called

--- test_case/BankingApplication.spec.ts
+++ test_case/BankingApplication.spec.ts
@@ -1,6 +1,14 @@
 import test from '@playwright/test'
+import { LoginPage } from '../pages/LoginPage'
+import { HomePage } from '../pages/HomePage'
 
 test('banking', async({page})=>{
 
     await page.goto("https://senthilsmartqahub.blogspot.com/2025/06/online-banking.html")
+
+    const loginpage = new LoginPage(page)
+    await loginpage.doLogin("Debit User","demo")
+    const homepage= new HomePage(page)
+    await homepage.verifywelcomemsg()
+
 })
