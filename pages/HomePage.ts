09ec8311c5b55286a370a640d7d80aef4991bb8d
Mohanraj <mohanmraj@rocketmail.com>
2026-07-22 14:31:32 +0530

    debugging

--- pages/HomePage.ts
+++ pages/HomePage.ts
@@ -1,11 +1,16 @@
-import { Locator, Page } from "@playwright/test";
+import { expect, Locator, Page } from "@playwright/test";
 
 export class HomePage{
     private welcomemessage: Locator
 
     constructor(page:Page)
     {
-        this.welcomemessage=page.getByText("Welcome to DebitUser")
+        this.welcomemessage=page.locator("welcomeUser")
     
+    }
+
+    async verifywelcomemsg()
+    {
+        await expect(this.welcomemessage).toHaveText("Welcome to DebitUser")
     }
 }
