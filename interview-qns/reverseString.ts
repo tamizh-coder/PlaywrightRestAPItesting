04d11f488978706f10485a70cb4e06ec55713e91
Mohanraj <mohanmraj@rocketmail.com>
2026-07-30 15:20:00 +0530

    reverse without space

--- interview-qns/reverseString.ts
+++ interview-qns/reverseString.ts
@@ -1,12 +1,28 @@
 //SDET Interview: Reverse String in TypeScript (Easy → Advanced)
 // Level 1: Reverse a String
 
-function reverseString(str:string):string{
-    let result:string="";
-    for (let i=str.length-1;i>=0; i--){
-        result+=str[i];
+// function reverseString(str:string):string{
+//     let result:string="";
+//     for (let i=str.length-1;i>=0; i--){
+//         result+=str[i];
+//     }
+//     //return str.split("String").reverse().join("");
+//     return result;
+// }
+// console.log(reverseString("moh an"))
+
+
+//Level 2: Ignore Spaces While Reversing
+function reversewithoutspace(str:string):string {
+    let result=""
+    for(let i=str.length-1; i>=0;i--)
+    {
+        if(str[i] !==" ")
+            {
+            result +=str[i];
+            }
     }
-    //return str.split("String").reverse().join("");
     return result;
+
 }
-console.log(reverseString("moh an"))
+console.log(reversewithoutspace("Mohan  raj script"))
